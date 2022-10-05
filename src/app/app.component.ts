fd907e735f7f31e405e8e76664e489c4dfb99db9
Leandro A. Siqueira <leandroaps@gmail.com>
2022-10-04 22:15:51 -0300

    Compliance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'How to select/unselect all checkbox';
-  masterSelected: boolean;
+  MASTER_SELECTED: boolean;
   CHECKLIST: any;
   CHECKED_LIST: any;
 
   constructor() {
-    this.masterSelected = false;
+    this.MASTER_SELECTED = false;
     this.CHECKLIST = [
-      { id: 1, value: 'Elenor Anderson', isSelected: false },
-      { id: 2, value: 'Caden Kunze', isSelected: true },
-      { id: 3, value: 'Ms. Hortense Zulauf', isSelected: true },
-      { id: 4, value: 'Grady Reichert', isSelected: false },
-      { id: 5, value: 'Dejon Olson', isSelected: false },
-      { id: 6, value: 'Jamir Pfannerstill', isSelected: false },
-      { id: 7, value: 'Aracely Renner DVM', isSelected: false },
-      { id: 8, value: 'Genoveva Luettgen', isSelected: false },
+      { id: 1, value: 'Elenor Anderson', IS_SELECTED: false },
+      { id: 2, value: 'Caden Kunze', IS_SELECTED: true },
+      { id: 3, value: 'Ms. Hortense Zulauf', IS_SELECTED: true },
+      { id: 4, value: 'Grady Reichert', IS_SELECTED: false },
+      { id: 5, value: 'Dejon Olson', IS_SELECTED: false },
+      { id: 6, value: 'Jamir Pfannerstill', IS_SELECTED: false },
+      { id: 7, value: 'Aracely Renner DVM', IS_SELECTED: false },
+      { id: 8, value: 'Genoveva Luettgen', IS_SELECTED: false },
     ];
     this.getCheckedItemList();
   }
@@ -29,15 +29,15 @@ export class AppComponent {
   // The master checkbox will check/ uncheck all items
   checkUncheckAll() {
     for (const element of this.CHECKLIST) {
-      element.isSelected = this.masterSelected;
+      element.IS_SELECTED = this.MASTER_SELECTED;
     }
     this.getCheckedItemList();
   }
 
   // Check All Checkbox Checked
   isAllSelected() {
-    this.masterSelected = this.CHECKLIST.every(function (item: any) {
-      return item.isSelected;
+    this.MASTER_SELECTED = this.CHECKLIST.every(function (item: any) {
+      return item.IS_SELECTED;
     });
     this.getCheckedItemList();
   }
@@ -46,7 +46,7 @@ export class AppComponent {
   getCheckedItemList() {
     this.CHECKED_LIST = [];
     for (const element of this.CHECKLIST) {
-      if (element.isSelected) this.CHECKED_LIST.push(element);
+      if (element.IS_SELECTED) this.CHECKED_LIST.push(element);
     }
     this.CHECKED_LIST = JSON.stringify(this.CHECKED_LIST);
   }
